2bd526d4a524f41dd8a2cded6a71a146f6dd5149
basilio007 <113118989+basilio007@users.noreply.github.com>
2023-10-19 17:08:38 +0200

    dfhdjk

--- proyectoBook4U/src/Clases/Pantalla_Principal.java
+++ proyectoBook4U/src/Clases/Pantalla_Principal.java
@@ -1,5 +1,6 @@
 package Clases;
 
+import java.awt.Color;
 import java.awt.Font;
 import java.awt.Image;
 
@@ -16,57 +17,22 @@ public class Pantalla_Principal extends JFrame{
 	private JPanel panel;
 
 	public static void main(String[] args) {
-		 java.awt.EventQueue.invokeLater(new Runnable() {
-			@Override
-			public void run() {
-				try {
-					Pantalla_Principal  frame = new Pantalla_Principal();
-					frame.setVisible(true);	
-				}catch (Exception e){
-					e.printStackTrace();
-				}
-				
-			    }
-			
-		        });
-	            }
-	
-	public Pantalla_Principal() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 1080, 720);
-		panel = new JPanel();
-		panel.setBorder(new EmptyBorder(5, 5, 5, 5));
-		panel.setLayout(null);
-		setTitle("Pantalla de Bienvenida");
 		
-		JLabel label = new JLabel();
-		Font font = new Font("Segoe Script", Font.TYPE1_FONT, 15);
-		label.setFont(font);
-		label.setBounds(330, 55, 100, 10);
-		panel.add(label);
+		JFrame frame = new JFrame();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(1080,720);
+		frame.setLocationRelativeTo(null);
+		frame.setLayout(null);
 		
-
-		
-		setContentPane(panel);
+		JPanel panel1 = new JPanel();
+		panel1.setBackground(Color.green);
+		panel1.setBounds(0, 0, 1080, 200);
 		
 		
-		setDefaultCloseOperation(EXIT_ON_CLOSE);//se cierra la ventana correctamente
+		frame.add(panel1);
+		frame.setVisible(true);
 		
-		JLabel label1 = new JLabel("PANTALLA PRINCIPAL!!");
+		JLabel label1 = new JLabel("PANTALLA PRINCIPAL");
 		
-		Font font1 = new Font("Verdana", Font.TYPE1_FONT, 20);
-		label1.setFont(font1);
-		label1.setBounds(360, 5, 500, 60);
-		add(label1);
-		setTitle("Book4You");
-		
-		JLabel etiqueta2 = new JLabel();
-		ImageIcon imagen  = new ImageIcon("Pictures/Logo Renuncios.png"); 
-		etiqueta2.setBounds(0,0,500,400);
-		etiqueta2.setIcon(new ImageIcon(imagen.getImage().getScaledInstance(500, 500, Image.SCALE_SMOOTH)));
-		add(etiqueta2);
-		
-		JButton btn = new JButton("MENÚ");
-	
 	}
 }
